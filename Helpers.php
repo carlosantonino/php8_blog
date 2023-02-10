68d3358dad77b19d22b4dc6b4a929129a0f1cde4
Carlos <72629502+carlosantonino@users.noreply.github.com>
2023-02-10 17:00:02 -0300

    comandos if

--- Helpers.php
+++ Helpers.php
@@ -1,11 +1,30 @@
 <?php
 
-function saudacao()
+function saudacao(): string  // O comando aposto os dois pontos define tipo de retorno
 {
-    return "Boa tarde<br>";
+    $hora = 19;
+
+    if($hora >= 0 && $hora <= 5){
+        $saudacao = 'Boa madrugada!!!<br>';
+    }
+
+    if($hora >= 6 && $hora <= 12){
+        $saudacao = 'Bom dia!!!<br>';
+    }
+
+    if($hora >= 13 && $hora <= 18){
+        $saudacao = 'Bom tarde!!!<br>';
+    }
+
+    if($hora >= 19 && $hora <= 23){
+        $saudacao = 'Bom noite!!!<br>';
+    }
+
+    return $saudacao;
+    
 }
 
-function resumirTexto($texto, $limite, $continue = '...')
+function resumirTexto(string $texto, int $limite, $continue = '...'): string
 {
     return $texto . " " . $limite;
 }
